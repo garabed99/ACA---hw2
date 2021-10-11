46a063011c5c0b6f3a3a8d439d864f8284fe4016
garabed99 <37003106+garabed99@users.noreply.github.com>
2021-10-11 16:32:41 +0400

    task2 committed

---
+++ task2.js
@@ -0,0 +1,13 @@
+function toAcronym(sentence) {
+    let splitSentence = sentence.trim().split(" ");
+    let acronymArr = [];
+
+    for(let i = 0; i < splitSentence.length; i++) {
+        acronymArr.push(splitSentence[i].charAt(0));
+    }
+
+    return acronymArr.join('').toUpperCase();
+} 
+
+console.log(toAcronym("Prisoner of War"));
+console.log(toAcronym("Have a good night"));
